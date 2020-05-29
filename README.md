a19cb610a2eba02c9091a6a59df979116d8cba64
helaris <45002132+helaris@users.noreply.github.com>
2020-05-29 12:02:04 +0200

    Update README.md & app.js

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 *Second JavaScript Project*
 
 In this project I'll be trying to create a movie seat booking UI with local storage.
+
+- First part: Selecting seats, adding classes, updating DOM
+- Second part: Adding local storage, when the page is refreshed it will keep the selected seats
--- app.js
+++ app.js
@@ -5,30 +5,74 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movies = document.getElementById('movie');
 
+populateUI();
+
 let ticketPrice = +movies.value;
 
-//3. Update count and total DOM
+//Save selected movie index and price
+function setMovieData(movieIndex, moviePrice){
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
+//Update count and total DOM
 function updateDOM(){
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+    // Copy selected seats into arr - seatsIndex / use spread operator ...selectedSeats
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+
+    //Save seatsIndex to local storage
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+    
+    
+    //Map through array
+    // return a new array of indexes
+
     const selectedSeatsCount = selectedSeats.length;
     
     count.innerHTML = selectedSeatsCount;
     total.innerHTML = selectedSeatsCount * ticketPrice;
 }
 
-//4. Pick a movie value and update DOM
+//Get data from localstorage and populate UI
+function populateUI(){
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+    if(selectedSeats !== null && selectedSeats.length > 0){
+        seats.forEach((seat, index) => {
+            if(selectedSeats.indexOf(index) > -1){
+                seat.classList.add('selected');
+            }
+        });
+    }
+
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+    if(selectedMovieIndex !== null){
+        movies.selectedIndex = selectedMovieIndex;
+    }
+}
+
+//Pick a movie value and update DOM
 movies.addEventListener('change', e => {
     ticketPrice = +e.target.value;
-    
-    updateDOM();
-})
+    // Getting movie index and value/price and passing it to setMovieData
+    setMovieData(e.target.selectedIndex, e.target.value);
 
-//2. Seat click event - if i click the seat that it will toggle success/ not occupied
+    updateDOM();
+});
+
+//Seat click event - if i click the seat that it will toggle success/ not occupied
 container.addEventListener('click', e => {
     if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')){
         e.target.classList.toggle('selected');
 
         updateDOM();
     }
-})
+});
 
+// Initial count and total set
+
+updateDOM();
