6fefd1d9799e75792513ee94f135ee1037821f19
helaris <45002132+helaris@users.noreply.github.com>
2020-05-29 10:45:46 +0200

    Add app.js

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
+//1. Take all the DOM elements
 const container = document.querySelector('.container');
 const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
+const movies = document.getElementById('movie');
 
-const movieSelect = document.getElementById('movie');
-let moviePrice = +movieSelect.value;
+let ticketPrice = +movies.value;
 
-
-//Update total and count
-function updateSelectedCount(){
+//3. Update count and total DOM
+function updateDOM(){
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
-    
     const selectedSeatsCount = selectedSeats.length;
-
+    
     count.innerHTML = selectedSeatsCount;
-    total.innerHTML = selectedSeatsCount * moviePrice;
+    total.innerHTML = selectedSeatsCount * ticketPrice;
 }
 
-//Movie Select event
-movieSelect.addEventListener('change', e => {
-    moviePrice = +e.target.value;
-    updateSelectedCount();
+//4. Pick a movie value and update DOM
+movies.addEventListener('change', e => {
+    ticketPrice = +e.target.value;
+    
+    updateDOM();
 })
 
-//Seat click event
+//2. Seat click event - if i click the seat that it will toggle success/ not occupied
 container.addEventListener('click', e => {
     if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')){
-        
         e.target.classList.toggle('selected');
 
-        updateSelectedCount();
+        updateDOM();
     }
-});
+})
+
